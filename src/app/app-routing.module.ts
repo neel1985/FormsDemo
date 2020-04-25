86241d0f41f2ec41d4b94276859cdd0efddbb284
unknown <arun.banik.1985@gmail.com>
2020-04-25 14:34:43 +0800

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  'template-form', component:  TemplateDrivenFormComponent},
+  { path:  'reactive-form', component:  ReactiveFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [TemplateDrivenFormComponent, ReactiveFormComponent]
 })
 export class AppRoutingModule { }
